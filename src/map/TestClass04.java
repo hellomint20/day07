bc2522194736b9663aa839cdd8d0c7732c689fbe
seohee <test@test.123>
2023-06-20 17:11:30 +0900

    LinkedHashMap

---
+++ src/map/TestClass04.java
@@ -0,0 +1,22 @@
+package map;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+public class TestClass04 {
+	public static void main(String[] args) {
+		//HashMap<String, Integer> map = new HashMap<>();
+		LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+		map.put("선풍기", 100);
+		map.put("자동차", 200);
+		map.put("에어컨", 300);
+		System.out.println(map);
+		
+		//Set<String> set = map.keySet();
+		//Iterator<String> it = set.iterator();
+		Iterator<String> it = map.keySet().iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+}
